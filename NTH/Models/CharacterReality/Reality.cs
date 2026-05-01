f421c1374dc2a7c5a4f1dacf5f54fef21bdc09fe
Kizuna Akari <49898006+LtCoinCidenCe@users.noreply.github.com>
2026-05-02 00:14:47 +0800

    Added more characters

--- NTH/Models/CharacterReality/Reality.cs
+++ NTH/Models/CharacterReality/Reality.cs
@@ -11,6 +11,15 @@ public static class Reality
 			new() { Nameue = "紲星", Nameshita = "あかり", Introduction = "明るい女の子の可愛らしい中にも優しさあふれる声" },
 			new() { Nameue = "琴葉", Nameshita = "茜", Introduction = "関西弁で喋る百合姉" },
 			new() { Nameue = "琴葉", Nameshita = "葵", Introduction = "標準語で喋る百合妹" },
+			new() { Nameue = "弦巻", Nameshita = "マキ", Introduction = "胸が大きいバンドメンバー" },
+			new() { Nameue = "宮舞", Nameshita = "モカ", Introduction = "もう上しか見えない歌手" },
+			new() { Nameue = "紡乃世", Nameshita = "詞音", Introduction = "角という名の、奇抜な髪型をした有名人" },
+			new() { Nameue = "双葉", Nameshita = "湊音", Introduction = "青春怪人" },
+			new() { Nameue = "夏色", Nameshita = "花梨", Introduction = "小樽市のパイセン" },
+			new() { Nameue = "小春", Nameshita = "六花", Introduction = "中国古詩" },
+			new() { Nameue = "花隈", Nameshita = "千冬", Introduction = "文芸眼鏡娘" },
+			new() { Nameue = "四国", Nameshita = "めたん", Introduction = "常に金欠高等部二年生" },
+			new() { Nameue = "春日部", Nameshita = "つむぎ", Introduction = "埼玉ギャル" },
 		];
 		CharactersByName = Characters.ToImmutableSortedDictionary(x => x.Nameue + x.Nameshita, x => x);
 	}
