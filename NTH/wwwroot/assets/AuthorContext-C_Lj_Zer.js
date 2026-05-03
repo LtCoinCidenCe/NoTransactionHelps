ae5e25a0df69518de80f6b63863266c8abd53974
Kizuna Akari <49898006+LtCoinCidenCe@users.noreply.github.com>
2026-05-03 09:52:05 +0800

    new static files for loading

---
+++ NTH/wwwroot/assets/AuthorContext-C_Lj_Zer.js
@@ -0,0 +1,1 @@
+import{a as t}from"./chunk-EPOLDU6W-BAwP1xvx.js";const r=t.createContext({authors:[],setAuthors:()=>{}});export{r as A};
---
+++ NTH/wwwroot/assets/JWTContext-BMJvHBrK.js
@@ -0,0 +1,1 @@
+import{a as e}from"./chunk-EPOLDU6W-BAwP1xvx.js";const t={jwt:"",expireTime:new Date(1990,2,3),userIdentifier:""},o=e.createContext(t);export{o as J};
