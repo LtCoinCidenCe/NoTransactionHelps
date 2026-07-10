ee65c2f374f09cd07b48271ff49a95c38f16422f
CoinCidenCe <49898006+LtCoinCidenCe@users.noreply.github.com>
2026-07-10 03:17:52 +0000

    character settings

--- NTH/Controllers/CharacterController.cs
+++ NTH/Controllers/CharacterController.cs
@@ -30,13 +30,43 @@ public class CharacterController(PostgresContext database) : ControllerBase
 			Nameue = newChara.Nameue,
 			Nameshita = newChara.Nameshita,
 			Introduction = newChara.Introduction,
-			Age = newChara.Age
+			Age = newChara.Age,
+			Mingzi = newChara.Mingzi,
+			Bieming = newChara.Bieming
 		};
 		database.CharacterReality.Add(newCharacter);
 		try { database.SaveChanges(); }
 		catch (DbUpdateException ex) when (ex.InnerException is not null && ex.InnerException.Message.StartsWith("23505"))
 		{ return BadRequest("Character already exists."); }
 		return CreatedAtAction(nameof(CreateCharacter), newCharacter);
+	}
+
+	[HttpPut]
+	public IActionResult ModifyCharacter(NewCharacter character)
+	{
+		CharacterID? existing = Reality.CharactersByName.FirstOrDefault(x => x.Key == character.Nameue + character.Nameshita).Value;
+		if (existing is not null)
+			return BadRequest("Fixed character");
+		existing = database.CharacterReality.FirstOrDefault(x => x.Nameue == character.Nameue && x.Nameshita == character.Nameshita);
+		if (existing is null)
+			return NotFound();
+		if (character.Introduction.Length > 0)
+			existing.Introduction = character.Introduction;
+		if (character.Age.HasValue)
+			existing.Age = character.Age;
+		if (character.Mingzi.Length > 0)
+			existing.Mingzi = character.Mingzi;
+		if (character.Bieming.Count > 0 || character.DeleteBieming.Count > 0)
+		{
+			SortedSet<string> xinbieming = [.. existing.Bieming, .. character.Bieming];
+			foreach (var item in character.DeleteBieming)
+			{
+				xinbieming.Remove(item);
+			}
+			existing.Bieming = [.. xinbieming];
+		}
+		database.SaveChanges();
+		return Ok("OK");
 	}
 }
 
@@ -49,4 +79,8 @@ public class NewCharacter
 	[MaxLength(500)]
 	public string Introduction { get; set; } = string.Empty;
 	public int? Age { get; set; }
+	[MaxLength(25)]
+	public string Mingzi { get; set; } = string.Empty;
+	public List<string> Bieming { get; set; } = [];
+	public List<string> DeleteBieming { get; set; } = [];
 }
--- NTH/Models/CharacterReality/CharacterID.cs
+++ NTH/Models/CharacterReality/CharacterID.cs
@@ -16,9 +16,17 @@ public class CharacterID
 	[MaxLength(500)]
 	public string Introduction { get; set; } = string.Empty;
 	public int? Age { get; set; }
+	[MaxLength(25)]
+	public string Mingzi { get; set; } = string.Empty;
+	public List<string> Bieming { get; set; } = [];
 	#endregion Database
 
 	#region Calculated Properties
+	/// <summary>
+	/// help the memory to mark it as a code stored character
+	/// </summary>
+	public bool fixedChara = false;
+	public bool IsFixed { get => fixedChara; }
 	public string FullName
 	{
 		get => Nameue + Nameshita;
