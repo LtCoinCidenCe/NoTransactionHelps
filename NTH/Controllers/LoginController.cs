90c83a9fb0914c579d568cdd7e2f78f53d0cb17d
Zigi Wang (external) <Zigi.Wang@wp.numerator.com>
2026-07-16 16:49:12 +0800

    work in progress

--- NTH/Controllers/LoginController.cs
+++ NTH/Controllers/LoginController.cs
@@ -9,9 +9,13 @@ using NTH.Middlewares;
 using NTH.Models.User;
 using NTH.Services;
 using NTH.Utilities;
+using System.Buffers.Text;
+using System.ComponentModel.DataAnnotations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 
 namespace NTH.Controllers;
 
@@ -53,6 +57,64 @@ public class LoginController(ILogger<LoginController> logger, SQLiteContext data
 		return Ok(token);
 	}
 
+	[HttpPost, Route("InvitationLink"), Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+	public async Task<IActionResult> GenerateInvitationLink([FromServices] RequestingUser requestingUser)
+	{
+		var dateTimeOffset = DateTimeOffset.UtcNow;
+		var clearText = new InvitationToken
+		{
+			ByUserAudit = requestingUser.UserID,
+			CreationDate = dateTimeOffset,
+		};
+		var jsonBytes = JsonSerializer.SerializeToUtf8Bytes(clearText);
+		var aes0 = Aes.Create();
+
+		var initializationVector = aes0.IV;
+		byte[] buffer = new byte[300];
+		var successful = aes0.TryEncryptCfb(jsonBytes, initializationVector, buffer, out var written);
+		if (!successful)
+		{
+			logger.LogError("encryption failed when preparing invitation token");
+			return Problem();
+		}
+		Array.Resize(ref buffer, written);
+		var salasana = Convert.ToBase64String(buffer, 0, written);
+
+		var dbInvite = new UserInvitationLink
+		{
+			ByUserAudit = requestingUser.UserID,
+			CreationDate = dateTimeOffset,
+			IV = initializationVector,
+		};
+
+		var linkBuilder = new UriBuilder
+		{
+			Scheme = Request.Scheme,
+			Host = Request.Host.ToUriComponent(),
+			Path = "Invited",
+			Query = $"?ID={0}&token={salasana}"
+		};
+		var ljnk = linkBuilder.ToString();
+
+		return Ok(ljnk);
+	}
+
+	[HttpGet, Route("InvitationTokenValidation")]
+	public async Task<IActionResult> InvitationTokenValidation
+		([FromQuery, Required] long ID, [FromQuery, Required, MinLength(14)] string token)
+	{
+		var salasana = Convert.FromBase64String(token);
+		var aes0 = Aes.Create();
+		return NotFound();
+	}
+
+	[HttpGet, Route("InvitedAccountCreation")]
+	public async Task<IActionResult> InvitedAccountCreation
+		([FromQuery, Required] long ID, [FromQuery, Required, MinLength(14)] byte[] token)
+	{
+		return NotFound();
+	}
+
 	[HttpDelete, Route("salainen/shutdown"), Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
 	public IActionResult ShutdownProgram([FromServices] RequestingUser requestingUser)
 	{
@@ -62,4 +124,10 @@ public class LoginController(ILogger<LoginController> logger, SQLiteContext data
 		Program.app.StopAsync();
 		return Ok("OK");
 	}
+}
+
+public class InvitationToken
+{
+	public long ByUserAudit { get; set; }
+	public DateTimeOffset CreationDate { get; set; }
 }
---
+++ NTH/Models/User/UserInvitationLink.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NTH.Models.User;
+
+public class UserInvitationLink
+{
+	public long ID { get; set; }
+	public long ByUserAudit { get; set; }
+	public DateTimeOffset CreationDate { get; set; } = DateTimeOffset.UtcNow;
+	public bool IsUsed { get; set; } = false;
+	/// <summary>
+	/// Encryption Initialization Vector
+	/// </summary>
+	[MaxLength(32)]
+	public required byte[] IV { get; set; }
+	[MaxLength(400)]
+	public string URLGenerated { get; set; } = "";
+	public long? CreatedUser { get; set; } = null;
+}
