140ce6be674cb66f0f8c34bb4281d8d5eafcc8e5
CoinCidenCe <49898006+LtCoinCidenCe@users.noreply.github.com>
2026-07-30 16:31:02 +0800

    database start log (#6)

---
+++ NTH/Models/NTH/ProgramStart.cs
@@ -0,0 +1,9 @@
+﻿namespace NTH.Models.NTH;
+
+public class ProgramStart
+{
+	public int ID { get; set; }
+	public DateTimeOffset StartTime { get; set; } = DateTimeOffset.UtcNow;
+	public bool IsFailed { get; set; } = false;
+	public string? ErrorMessage { get; set; } = null;
+}
