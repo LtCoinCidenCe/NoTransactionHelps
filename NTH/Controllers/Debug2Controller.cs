dcb91ecff6739d7bc185dde319a6b18d76444615
Kizuna Akari <49898006+LtCoinCidenCe@users.noreply.github.com>
2026-08-22 16:14:03 +0800

    wip

--- NTH/Controllers/Debug2Controller.cs
+++ NTH/Controllers/Debug2Controller.cs
@@ -2,9 +2,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using NTH.DBContext;
+using NTH.dlpJSONs;
 using NTH.Models.User;
 using NTH.Services;
 using NTH.Utilities;
+using System.Text.Json;
 
 namespace NTH.Controllers;
 
@@ -41,6 +43,19 @@ public class Debug2Controller(SQLiteContext database) : ControllerBase
 			.ThenInclude(x => x.Video)
 			.ToList();
 		return users;
+	}
+
+	[HttpGet]
+	[Route("TryParseJson")]
+	public async Task<IActionResult> TryParse()
+	{
+		var filename = @"D:\VideoProjects\轴\information爬虫\dlpdata\【voiceroid劇場】この中で一番付き合いたいと思ってる女の子は誰でしょうか！！！ [sm44910136].info.json";
+		var extended = filename.Split(' ').LastOrDefault();
+		if (string.IsNullOrEmpty(extended))
+			throw new NTHException("not valid truth json file");
+		var jsonStream = System.IO.File.OpenRead(filename);
+		var theObject = await JsonSerializer.DeserializeAsync<VideoNicoTruth>(jsonStream);
+		return Ok("OK");
 	}
 }
 
