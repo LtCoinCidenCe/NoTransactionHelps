fc99c71d55feb0430fb13edc84617c42814cbdb5
Kizuna Akari <49898006+LtCoinCidenCe@users.noreply.github.com>
2026-09-25 18:00:44 +0800

    dlp path local remove fix

--- NTH/Utilities/PasswordHasher.cs
+++ NTH/Utilities/PasswordHasher.cs
@@ -5,23 +5,23 @@ namespace NTH.Utilities;
 
 public static class PasswordHasher
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <param name="password">password clear text, no check here</param>
-    /// <param name="salt">pass null to generate a salt or pass existing salt</param>
-    /// <returns>hashed byte array of password</returns>
-    public static byte[] GetHashedPassword(string password, ref string? salt)
-    {
-        if (string.IsNullOrEmpty(salt))
-        {
-            salt = RandomASCIIGenerator.GetString(5);
-        }
-        byte[] hash = Encoding.UTF8.GetBytes(salt + password);
-        for (int i = 0; i < 5; i++)
-        {
-            hash = SHA256.HashData(hash);
-        }
-        return hash;
-    }
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="password">password clear text, no check here</param>
+	/// <param name="salt">pass null to generate a salt or pass existing salt</param>
+	/// <returns>hashed byte array of password</returns>
+	public static byte[] GetHashedPassword(string password, ref string? salt)
+	{
+		if (string.IsNullOrEmpty(salt))
+		{
+			salt = RandomASCIIGenerator.GetString(5);
+		}
+		byte[] hash = Encoding.UTF8.GetBytes(salt + password);
+		for (int i = 0; i < 5; i++)
+		{
+			hash = SHA256.HashData(hash);
+		}
+		return hash;
+	}
 }
--- NTH/Utilities/RandomASCIIGenerator.cs
+++ NTH/Utilities/RandomASCIIGenerator.cs
@@ -4,17 +4,17 @@ namespace NTH.Utilities;
 
 public static class RandomASCIIGenerator
 {
-    private static Random random = new();
-    public static string GetString(int length)
-    {
-        StringBuilder builder = new(length);
-        int minPrintable = 0x21;
-        // int chars = 95;
-        for (int i = 0; i < length; i++)
-        {
-            int letter = minPrintable + random.Next() % 95;
-            builder.Append((char)letter);
-        }
-        return builder.ToString();
-    }
+	private static Random random = new();
+	public static string GetString(int length)
+	{
+		StringBuilder builder = new(length);
+		int minPrintable = 0x21;
+		// int chars = 95;
+		for (int i = 0; i < length; i++)
+		{
+			int letter = minPrintable + random.Next() % 95;
+			builder.Append((char)letter);
+		}
+		return builder.ToString();
+	}
 }
